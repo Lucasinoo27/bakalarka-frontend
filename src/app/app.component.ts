e9d2f2749c677e1f63e647b263a28152260aaf85
Lucasinoo27 <lukas.sutka123@gmail.com>
2026-03-28 15:26:31 +0100

    Prvy commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,27 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  standalone: true,
+  imports: [CommonModule],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'recepty-app';
+export class AppComponent implements OnInit {
+  recepty: any[] =[];
+
+  constructor(private http: HttpClient) {}
+
+  ngOnInit() {
+    // Ťaháme dáta z nášho Node.js backendu
+    this.http.get<any[]>('http://localhost:3000/api/recepty').subscribe({
+      next: (data) => {
+        this.recepty = data;
+        console.log('Stiahnuté recepty:', this.recepty);
+      },
+      error: (err) => console.error('Chyba pri sťahovaní:', err)
+    });
+  }
 }
