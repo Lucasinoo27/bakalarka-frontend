a220c29b310b19ba8f0c33b0c607130c08e35f4d
Lucasinoo27 <lukas.sutka123@gmail.com>
2026-03-28 17:20:32 +0100

    revert

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
@@ -8,21 +8,6 @@ import { RouterOutlet } from '@angular/router';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent implements OnInit {
+export class AppComponent {
   // Tento komponent slúži už len ako root layout (prázdna obálka pre routing)
-  
-  ngOnInit() {
-    // Junior Anti-pattern (Best Practices violation):
-    // Pýtame sa na polohu (Geolocation) používateľa ihneď pri prvom načítaní aplikácie, bez jeho vyžiadania
-    if (typeof window !== 'undefined' && navigator.geolocation) {
-      navigator.geolocation.getCurrentPosition(
-        (position) => {
-          console.log('Poloha získaná (Junior dev chcel zbierať dáta pre analytiku): ', position);
-        },
-        (error) => {
-          console.log('Používateľ zamietol polohu alebo nastala chyba: ', error);
-        }
-      );
-    }
-  }
 }
